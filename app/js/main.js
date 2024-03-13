400a43ae74458efe93101d5198cdffbe19a66861
oilprint <irynakozlova.k@gmail.com>
2024-03-13 12:04:11 +0200

    adaptive footer, obile-menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
       //Mobile Menu
-    const burger = document.querySelector('.header__burger');
+    const burger = document.querySelector('.header__mobile-btn');
 
     console.log('test');
     const mobileMenu = document.querySelector('.mobile-menu'); 
     const closeButton = document.querySelector('.mobile-menu__button');
-    const bodyLock = document.querySelector('body'); 
+    const bodyLock = document.querySelector('.wrapper'); 
 
     burger.addEventListener('click', () => {
       mobileMenu.classList.add('mobile-menu--active'); 
@@ -18,11 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
       mobileMenu.classList.remove('mobile-menu--active'); 
       bodyLock.classList.remove('lock');
     });
-
-
-
-   
-
 });
 
 
